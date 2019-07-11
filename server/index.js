1aade864a80c9b764fe56d9f656e36f5b3e1ae42
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 13:01:09 +0300

    [FT] Change server js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(
+    express.static(
+        path.join(__dirname, '../build'),
+        { index: false },
+    ),
+);
 
 app.get('/api/posts', (req, res) => {
     res.send(postsList);
