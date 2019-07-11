bb67fd3d5c1c13fbab4615fc2e049eaba189ba9f
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-11 12:42:18 +0300

    [FT] Remove express-pino-logger

--- server/index.js
+++ server/index.js
@@ -11,12 +11,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(pino);
 
 app.get('/api/posts', (req, res) => {
     res.send(postsList);
