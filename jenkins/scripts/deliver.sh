f12b1fa8730bead58d3e687589e3905355db549a
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-12 12:17:04 +0300

    [FT] Jenkins file add test stage

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -21,16 +21,16 @@ set +x
 #
 
 # exit 1 on errors
-set -e
+#set -e
 
 # deal with remote
-echo "Checking if remote exists..."
-if ! git ls-remote heroku; then
-  echo "Adding heroku remote..."
-  git remote add heroku git@heroku.com:radiant-river-46437.git
-fi
+#echo "Checking if remote exists..."
+#if ! git ls-remote heroku; then
+ # echo "Adding heroku remote..."
+  #git remote add heroku git@heroku.com:radiant-river-46437.git
+#fi
 
 # push only origin/master to heroku/master - will do nothing if
 # master doesn't change.
-echo "Updating heroku master branch..."
-git push heroku origin/master:master
+#echo "Updating heroku master branch..."
+#git push heroku origin/master:master
