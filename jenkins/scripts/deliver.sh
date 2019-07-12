15024f367abbe58bc73e172fdf3856d0f60b95bc
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-12 11:40:47 +0300

    [FT] Change deliver sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,4 +8,29 @@ echo 'the best performance.'
 set -x
 npm run build
 set +x
-git push https://git.heroku.com/radiant-river-46437.git HEAD:master
+
+#!/bin/bash
+#
+# login in the jenkins server with:
+#
+# heroku login
+# heroku keys:add
+#
+# Doing so, jenkins will have permission to deploy to
+# the heroku remote.
+#
+
+# exit 1 on errors
+set -e
+
+# deal with remote
+echo "Checking if remote exists..."
+if ! git ls-remote heroku; then
+  echo "Adding heroku remote..."
+  git remote add heroku git@heroku.com:radiant-river-46437.git
+fi
+
+# push only origin/master to heroku/master - will do nothing if
+# master doesn't change.
+echo "Updating heroku master branch..."
+git push heroku origin/master:master
